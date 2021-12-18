b5ae3c98504bde60215d6c7cb1e33b713752d774
Pete Scheidler <pscheidler@gmail.com>
2021-12-18 16:23:21 -0500

    It works!

--- src/day03.rs
+++ src/day03.rs
@@ -1,9 +1,11 @@
 use std::fs::File;
 use std::io::{self, BufRead, Lines, BufReader};
 use std::convert::TryFrom;
+use std::fmt::Error;
 // use crate::day03::bit_criteria::o2;
 
 #[derive(PartialEq)]
+#[derive(Copy, Clone)]
 enum BitCriteria {
     o2,
     co2,
@@ -17,16 +19,17 @@ fn filter_list(list_in: &Vec<u32>, index: u8, kind: BitCriteria) -> Vec<u32> {
 
     for value in list_in {
         if (value & mask) > 0 {
-            println!("1: {} ({})", value, one.len());
+            // println!("1: {} ({})", value, one.len());
             one.push(*value);
         } else {
-            println!("0: {}, ({})", value, zero.len());
+            // println!("0: {}, ({})", value, zero.len());
             zero.push(*value);
         }
     }
-    let keep_one :bool = one.len() > zero.len();
-    let is_o2 :bool = kind == BitCriteria::o2;
-    if keep_one ^ is_o2 {
+    println!("1s = {}, 0s = {}", one.len(), zero.len());
+    let keep_one :bool = one.len() >= zero.len();
+    let is_co2 :bool = kind == BitCriteria::co2;
+    if keep_one ^ is_co2 {
         println!("Keeping the ones");
         return one;
     }
@@ -34,44 +37,42 @@ fn filter_list(list_in: &Vec<u32>, index: u8, kind: BitCriteria) -> Vec<u32> {
     return zero;
 }
 
-fn file_to_vec(input: &mut Lines<BufReader<File>>) -> Vec<u32> {
+fn file_to_vec(file_name: &str) -> std::io::Result<(Vec<u32>, i32)> {
+    let file = File::open(file_name)?;
+    let mut lines = io::BufReader::new(file).lines();
     let mut ret_val :Vec<u32> = Vec::new();
-    for line in input {
+    let mut line_len :i32 = 0;
+    for line in lines {
         let line = line.unwrap();
         let parsed = u32::from_str_radix(line.as_str(), 2).unwrap();
+        line_len = line.len() as i32;
         ret_val.push(parsed)
     }
-    return ret_val;
+    Ok((ret_val, line_len))
 }
 
-fn get_value(file_name: std::str, kind: BitCriteria) -> std::io::Result<(i32)> {
+fn get_value(file_name: &str, kind: BitCriteria) -> std::io::Result<i32> {
+    let (mut data_list, bit_count) = file_to_vec(file_name)?;
 
-}
-
-pub fn part2() -> std::io::Result<()> {
-    let file = File::open("InputData_03.txt")?;
-    let mut lines = io::BufReader::new(file).lines();
-
-    // let mut fpeek = lines.peekable();
-    // let l1 = fpeek.peek();
-    // let l1_str = l1.clone();
-    // println!("L1 = {}", l1_str);
-    // let l1_len = l1_str.len();
-    // let bit_count :u8 = l1_len as u8;
-
-    let bit_count = 12;
-    println!("Lines are {} chars long", bit_count);
-
-    let mut data_list = file_to_vec(&mut lines);
+    println!("Found length of {}", bit_count);
 
     for index in (0..(bit_count)).rev() {
-        data_list = filter_list(&data_list, index, BitCriteria::o2);
+        data_list = filter_list(&data_list, index as u8, kind);
         println!("Checked bit {}, {} items remain", index, data_list.len());
         if data_list.len() == 1 {
             println!("Got {}", data_list[0]);
+            return Ok(data_list[0] as i32)
         }
     }
+    panic!("Didn't find any value!")
+}
 
+pub fn part2() -> std::io::Result<()> {
+    let input_file = "InputData_03.txt";
+    let o2Value  = get_value(&input_file, BitCriteria::o2)?;
+    let co2Value  = get_value(&input_file, BitCriteria::co2)?;
+    let result = o2Value * co2Value;
+    println!("Result is {}", result);
     Ok(())
 }
 
